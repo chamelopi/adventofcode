6aa7140a524d9351e68e3a649a4f7c39eca98f57
chamelopi <ben.gorny@web.de>
2022-12-11 18:05:09 +0100

    Complete part1 of day3

--- src/main/kotlin/day3/Main.kt
+++ src/main/kotlin/day3/Main.kt
@@ -1,16 +1,19 @@
 package day3
 
+import util.loadInput
+import util.sep
+
 fun getCompartments(rucksack: String): Pair<String, String> {
     val half = rucksack.length / 2;
     return Pair(rucksack.substring(0, half), rucksack.substring(half))
 }
 
 fun getSharedItems(compartments: Pair<String, String>): List<Char> {
-    return compartments.first.filter { compartments.second.contains(it) }
-        .toCharArray().toList()
+    return compartments.first.toList().filter { compartments.second.contains(it) }
+        .distinct()
 }
 
-val keys = ('a' .. 'z') + ('A' .. 'z')
+val keys = ('a' .. 'z') + ('A' .. 'Z')
 val values = (1..52)
 val itemPriorities = keys.zip(values).toMap()
 
@@ -18,6 +21,14 @@ fun getPriorityOfItem(item: Char): Int {
     return itemPriorities.getValue(item)
 }
 
+fun part1(): Int {
+    return loadInput("day3-input.txt")
+        .split(sep)
+        .map(::getCompartments)
+        .map(::getSharedItems)
+        .sumOf { it.map(::getPriorityOfItem).sum() }
+}
+
 fun main() {
-    // TODO
+    println(part1())
 }
